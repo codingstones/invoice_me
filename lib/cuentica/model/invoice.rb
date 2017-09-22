4bc56aaabcdb91a2e54cf55a452eba6703099bd2
Dani Latorre <dani@danilat.com>
2017-09-22 12:17:21 +0200

    total amount calculation to Invoice

--- lib/cuentica/model/invoice.rb
+++ lib/cuentica/model/invoice.rb
@@ -4,6 +4,23 @@ module Cuentica
 
     def initialize(args)
       @id = args["id"]
+      @expense_lines = args[:expense_lines]
+    end
+
+    def total_amount
+      total_amount = 0
+      @expense_lines.each do |expense|
+        base = expense[:base]
+        vat = expense[:vat]
+        retention = expense[:retention]
+
+        vat_amount = base*vat/100
+        retention_amount = base*retention/100
+
+        amount = base + (vat_amount - retention_amount)
+        total_amount += amount
+      end
+      total_amount
     end
   end
 end
---
+++ spec/cuentica/model/invoice_spec.rb
@@ -0,0 +1,12 @@
+describe "Invoice" do
+  let(:some_expense_lines) do
+    [
+      {base: 1000, vat: 21, retention: 15}
+    ]
+  end
+  it "calculates total amount" do
+    invoice = Cuentica::Invoice.new(expense_lines: some_expense_lines)
+
+    expect(invoice.total_amount).to eq 1060
+  end
+end
