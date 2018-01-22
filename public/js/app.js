d882b32f0f2a215124c43adbbb5e8e86fe30abde
Dani Latorre <dani@danilat.com>
2018-01-22 14:01:31 +0100

    lines addition when a invoice is registered is all moved to js

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,40 @@
-const LINE_TEMPLATE = `
+const line = (vat, retention, hasDeleteLine) => {
+  return `
   <p class="description"><label>Descripción:</label><input type="text" name="description[]"></p>
   <p><label>Base:</label><input type="number" name="base[]"></p>
-  <p><label>IVA:</label><input type="text" name="vat[]" value="21"></p>
-  <p><label>Retención:</label><input type="text" name="retention[]" value="15"></p>
-  <p class="delete"><a href="">Eliminar</a></p>
-`;
+  <p><label>IVA:</label><input type="text" name="vat[]" value="${vat}"></p>
+  <p><label>Retención:</label><input type="text" name="retention[]" value="${retention}"></p>
+  ${deleteLine(hasDeleteLine)}`;
+}
+
+const deleteLine = (showDeleteAnchor) => {
+  if (showDeleteAnchor) {
+    return '<p class="delete"><a href="">Eliminar</a></p>';
+  } else {
+    return '<p class="delete"></p>';
+  }
+}
 
 function addNewLine(){
   const lines = document.querySelector('#lines');
   const newLine = document.createElement("li");
-  newLine.innerHTML = LINE_TEMPLATE;
+  const hasMoreChilds = (lines.childElementCount > 0)
+  newLine.innerHTML = line(21, 15, hasMoreChilds);
+  if (hasMoreChilds) {
+    addDeleteLineListener(newLine);
+  }
+  lines.appendChild(newLine);
+}
+
+function addDeleteLineListener(newLine) {
   newLine.querySelector('a').addEventListener('click', (event) => {
     event.preventDefault();
     newLine.parentElement.removeChild(newLine);
   });
-  lines.appendChild(newLine)
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  addNewLine();
   const adder = document.querySelector('#adder');
   adder.addEventListener('click', (event) => {
     event.preventDefault();
