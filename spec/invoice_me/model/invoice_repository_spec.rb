d8ca9cb622a6744b4dbc55d8ab498ab730004e02
Dani Latorre <dani@danilat.com>
2018-01-25 20:11:56 +0100

    now invoice repository spec uses VCR to be more integration testing style

--- spec/invoice_me/model/invoice_repository_spec.rb
+++ spec/invoice_me/model/invoice_repository_spec.rb
@@ -1,7 +1,7 @@
 describe "Invoice Repository" do
-  let(:a_document_number) {'foo'}
   let(:a_provider_id) { 348489 }
-  let(:an_invoice_id) {'an id'}
+
+  let(:a_document_number) {'foo'}
   let(:a_date) {"2017-07-05"}
   let(:a_line) {
     {description: "a expense", base:1000, vat:21, retention:15}
@@ -11,16 +11,6 @@ describe "Invoice Repository" do
     InvoiceMe::Invoice.new(document_number: a_document_number,
     lines:[a_line])
   }
-
-  let(:a_cuentica_success_response) do
-    {
-      "id"=>an_invoice_id,
-      "date"=> a_date,
-      "provider"=>{"id"=>a_provider_id},
-      "document_number"=> a_document_number,
-      "expense_lines"=>[{"description"=>a_line[:description], "base"=>a_line[:base], "vat"=>a_line[:vat], "retention"=>a_line[:retention]}]
-    }
-  end
 
   let(:a_cuentica_error_response) do
     {"message"=>"A server error occurred, please try again later"}
@@ -34,25 +24,14 @@ describe "Invoice Repository" do
   context "puts an invoice" do
     context "with success" do
       before(:each) do
-        allow(@cuentica_client).to receive(:register_expense).with(an_instance_of(Hash)).and_return(a_cuentica_success_response)
-
-        @new_invoice = @invoice_repository.put(an_invoice)
+        @invoice_repository = InvoiceMe::InvoiceRepository.new(InvoiceMe::CuenticaClient.new)
+        VCR.use_cassette("add_invoice") do
+          @new_invoice = @invoice_repository.put(an_invoice)
+        end
       end
 
       it "is stored" do
-        expect(@new_invoice.id).to eq an_invoice_id
-      end
-
-      it "has all their attributes" do
-        expect(@new_invoice.document_number).to eq a_document_number
-        expect(@new_invoice.date).to eq a_date
-        expect(@new_invoice.provider_id).to eq a_provider_id
-
-        line = @new_invoice.lines.first
-        expect(line.description).to eq a_line[:description]
-        expect(line.base).to eq a_line[:base]
-        expect(line.vat).to eq a_line[:vat]
-        expect(line.retention).to eq a_line[:retention]
+        expect(@new_invoice.id).not_to be_nil
       end
     end
 
@@ -69,13 +48,16 @@ describe "Invoice Repository" do
 
   context "get invoices by provider" do
     before(:each) do
-      allow(@cuentica_client).to receive(:get_expenses).and_return([a_cuentica_success_response])
+      @invoice_repository = InvoiceMe::InvoiceRepository.new(InvoiceMe::CuenticaClient.new)
     end
 
     it "get a list" do
-      @invoices = @invoice_repository.find_by_provider(a_provider_id)
+      VCR.use_cassette("find_expenses") do
+        invoices = @invoice_repository.find_by_provider(a_provider_id)
 
-      expect(@invoices.first.id).to eq a_cuentica_success_response["id"]
+        expect(invoices).not_to be_empty
+        expect(invoices.first.provider_id).to eq a_provider_id
+      end
     end
   end
 end
